78605e8c50465b57e13583bb7154050690e4bc6f
Gustavo Ortega <gustavo.ortega.palacios@gmail.com>
2024-04-13 12:19:17 -0500

    [UPD] web: Adding image and description to products

--- onlyflans_dj/web/views.py
+++ onlyflans_dj/web/views.py
@@ -3,10 +3,10 @@ from django.shortcuts import render
 from django.template import loader
 
 PRODUCTS = [
-    {"id": 1, "name": "Product1", "price": 10.5},
-    {"id": 2, "name": "Product2", "price": 8.5},
-    {"id": 3, "name": "Product3", "price": 5.0},
-    {"id": 4, "name": "Product4", "price": 4.5}
+    {"id": 1, "name": "Product1", "price": 10.5, "picture": "https://picsum.photos/seed/picsum/200/300", "description": "Product's description 1"},
+    {"id": 2, "name": "Product2", "price": 8.5, "picture": "https://picsum.photos/id/237/200/300", "description": "Product's description 2"},
+    {"id": 3, "name": "Product3", "price": 5.0, "picture": "https://picsum.photos/200/300?grayscale", "description": "Product's description 3"},
+    {"id": 4, "name": "Product4", "price": 4.5, "picture": "https://picsum.photos/200/300/?blur", "description": "Product's description 4"}
 ]
 
 def index(request):
